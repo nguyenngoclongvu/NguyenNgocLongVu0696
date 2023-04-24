1042e9213b5e502f138742ba5826864b394eabde
nguyenngoclongvu <longvu02072001@gmail.com>
2023-04-24 15:11:04 +0700

    first commit

---
+++ NguyenNgocLongVu0696/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenNgocLongVu0696.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
